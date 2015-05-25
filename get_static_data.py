b83676b88dffa9003796448acf3ca91c90a6d3bd
raphaelrieuhelft <raphael.rieuhelft@gmail.com>
2015-05-25 15:50:47 +0100

    Add LDA inputs and first example

--- get_static_data.py
+++ get_static_data.py
@@ -5,14 +5,29 @@ f = open('riot_api_key', 'r')
 key = json.load(f)
 f.close()
 
-def getIdToName(riot:RiotWatcher):
-    champsById = riot.static_get_champion_list(data_by_id=True)['data']
-    idToName = dict()
-    for champ in champsById:
-            idToName[champsById[champ]['id']]=champsById[champ]['name']
-    d = open('id_to_name','w')
-    json.dump(idToName, d)
+
+def get_id_to_name(riot:RiotWatcher):
+    champs_by_id = riot.static_get_champion_list(data_by_id=True)['data']
+    id_to_name = dict()
+    for champ in champs_by_id:
+        id_to_name[champs_by_id[champ]['id']] = champs_by_id[champ]['name']
+    d = open('id_to_name', 'w')
+    json.dump(id_to_name, d)
     d.close()
+    return id_to_name
+
+
+def make_columns_to_id(id_to_name):
+    columns_to_id = dict()
+    c = 0
+    for id in id_to_name:
+        columns_to_id[c] = id
+        c = c + 1
+    c = open('columns_to_id', 'w')
+    json.dump(columns_to_id, c)
+    c.close()
+    return columns_to_id
 
 if __name__ == '__main__':
-    getIdToName(RiotWatcher(key))
+    m=get_id_to_name(RiotWatcher(key))
+    make_columns_to_id(m)
