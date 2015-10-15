0c6b5168271ff53e947d6566b4ca136ea26bee42
dianegalloiswong <diane.gallois-wong@ens.fr>
2015-10-16 00:34:32 +0200

    Drafts about GUI to run LDA

---
+++ app_test.py
@@ -0,0 +1,235 @@
+import tkinter
+# import tkinter.messagebox
+# tkinter.messagebox.showerror("Error","Invalid number of topics")
+
+
+# http://sebsauvage.net/python/gui/
+class simpleapp_tk(tkinter.Tk):
+    def __init__(self, parent):
+        tkinter.Tk.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+
+        self.entryVariable = tkinter.StringVar()
+        self.entry = tkinter.Entry(self, textvariable=self.entryVariable)
+        self.entry.grid(column=0, row=0, sticky='EW')
+        self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u"Enter text here.")
+
+        button = tkinter.Button(self, text=u"Click me !",
+                                command=self.OnButtonClick)
+        button.grid(column=1, row=0)
+
+        self.labelVariable = tkinter.StringVar()
+        label = tkinter.Label(self, textvariable=self.labelVariable,
+                              anchor="w", fg="white", bg="blue")
+        label.grid(column=0, row=1, columnspan=2, sticky='EW')
+        self.labelVariable.set(u"Hello !")
+
+        self.grid_columnconfigure(0, weight=1)
+        self.resizable(True, False)
+        self.update()
+        self.geometry(self.geometry())
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+    def OnButtonClick(self):
+        self.labelVariable.set(self.entryVariable.get() + " (You clicked the button)")
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+    def OnPressEnter(self, event):
+        self.labelVariable.set(self.entryVariable.get() + " (You pressed ENTER)")
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+
+"""
+app = simpleapp_tk(None)
+app.title('my application')
+app.mainloop()
+"""
+
+
+
+
+b_app = True
+
+class IntExpectedException(Exception):
+    def __init__(self, message, entry):
+        self.message = message
+        self.entry = entry
+
+def get_int_from_StringVar(stringvar,error_message=None,entry=None):
+    try:
+        return int(stringvar.get())
+    except ValueError:
+        raise IntExpectedException(error_message,entry)
+
+
+class App(tkinter.Tk):
+    def __init__(self, parent):
+        tkinter.Tk.__init__(self, parent)
+        self.parent = parent
+
+        self.lda_frame = tkinter.Frame(self)
+        self.lda_frame.grid()
+
+        self.lda_label = tkinter.Label(self.lda_frame, text="LDA parameters")
+        self.lda_label.grid(row=0,column=0,sticky='EW')
+
+        self.lda_params_frame = tkinter.Frame(self.lda_frame)
+        self.lda_params_frame.grid(row=1,column=0,sticky='EW')
+
+        self.lda_n_topics_var = tkinter.StringVar()
+        self.lda_n_topics_var.set("10")
+        lda_n_topics_label = tkinter.Label(self.lda_params_frame, text="number of topics")
+        lda_n_topics_label.grid(row=0,column=0)
+        self.lda_n_topics_entry = tkinter.Entry(self.lda_params_frame,
+                                                textvariable=self.lda_n_topics_var,
+                                                width=3)
+        self.lda_n_topics_entry.grid(row=0,column=1)
+
+        lda_button = tkinter.Button(self.lda_frame, text="Run LDA",
+                                    command=self.run_lda)
+        lda_button.grid(column=0, row=3)
+        self.lda_run_label_var = tkinter.StringVar()
+        lda_run_label = tkinter.Label(self.lda_frame, textvariable=self.lda_run_label_var)
+        lda_run_label.grid(column=0, row=4,sticky='EW')
+
+    def run_lda(self):
+        try:
+            n_topics = get_int_from_StringVar(self.lda_n_topics_var,
+                                              error_message="Invalid number of topics.",
+                                              entry=self.lda_n_topics_entry)
+            self.lda_run_label_var.set('Running LDA with {} topics.'.format(n_topics))
+        except IntExpectedException as e:
+            self.lda_run_label_var.set(e.message)
+            e.entry.focus_set()
+            e.entry.selection_range(0, tkinter.END)
+        # try:
+        #     n_topics = int(self.lda_n_topics_var.get())
+        #     self.lda_run_label_var.set('Running LDA with {} topics.'.format(n_topics))
+        # except ValueError:
+        #     self.lda_run_label_var.set("Invalid number of topics")
+        #     self.lda_n_topics_entry.focus_set()
+        #     self.lda_n_topics_entry.selection_range(0, tkinter.END)
+
+
+
+
+
+def launch_app():
+    app = App(None)
+    app.title('App')
+    app.mainloop()
+
+
+
+class TestApp(tkinter.Tk):
+    def __init__(self, parent):
+        tkinter.Tk.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+
+        self.entryVariable = tkinter.StringVar()
+        self.entry = tkinter.Entry(self, textvariable=self.entryVariable)
+        self.entry.grid(column=0, row=0, sticky='EW')
+        self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u"Enter text here.")
+
+        button = tkinter.Button(self, text=u"Click me !",
+                                command=self.OnButtonClick)
+        button.grid(column=1, row=0)
+
+        self.labelVariable = tkinter.StringVar()
+        label = tkinter.Label(self, textvariable=self.labelVariable,
+                              anchor="w", fg="white", bg="blue")
+        label.grid(column=0, row=1, columnspan=2, sticky='EW')
+        self.labelVariable.set(u"Hello !")
+
+
+
+
+        rbuttons = tkinter.Frame(self)
+        rbuttons.grid(column=0,row=2)
+        self.rvar = tkinter.IntVar()
+        self.rlabelvar = tkinter.StringVar()
+        r1 = tkinter.Radiobutton(rbuttons, text="Option1",
+                                 variable=self.rvar, value=1,
+                                 command=self.rselect)
+        r1.grid(column=0,row=0)
+        r2 = tkinter.Radiobutton(rbuttons, text="Option2",
+                                 variable=self.rvar, value=2,
+                                 command=self.rselect)
+        r2.grid(column=0,row=1)
+        r3 = tkinter.Radiobutton(rbuttons, text="Option3",
+                                 variable=self.rvar, value=3,
+                                 command=self.rselect)
+        r3.grid(column=0,row=2)
+        self.rlabel = tkinter.Label(rbuttons, textvariable=self.rlabelvar)
+        self.rlabel.grid(column=0,row=3)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.resizable(True, False)
+        #self.update()
+        #self.geometry(self.geometry())
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+    def rselect(self):
+        selection = "You selected the option " + str(self.rvar.get())
+        #self.rlabel.config(text=selection)
+        self.rlabelvar.set(selection)
+
+    def OnButtonClick(self):
+        self.labelVariable.set(self.entryVariable.get() + " (You clicked the button)")
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+    def OnPressEnter(self, event):
+        self.labelVariable.set(self.entryVariable.get() + " (You pressed ENTER)")
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+def launch_TestApp():
+    tapp = TestApp(None)
+    tapp.title('TestApp')
+    tapp.mainloop()
+
+
+if __name__ == "__main__":
+    if b_app:
+        launch_app()
+    else:
+        launch_TestApp()
+
+"""
+top = tkinter.Tk()
+CheckVar1 = tkinter.IntVar()
+CheckVar2 = tkinter.IntVar()
+C1 = tkinter.Checkbutton(top, text = "Music", variable = CheckVar1, \
+                 onvalue = 1, offvalue = 0, height=5, \
+                 width = 20)
+C2 = tkinter.Checkbutton(top, text = "Video", variable = CheckVar2, \
+                 onvalue = 1, offvalue = 0, height=5, \
+                 width = 20)
+C1.pack()
+C2.pack()
+Lb1 = tkinter.Listbox(top)
+Lb1.insert(1, "Python")
+Lb1.insert(2, "Perl")
+Lb1.insert(3, "C")
+Lb1.insert(4, "PHP")
+Lb1.insert(5, "JSP")
+Lb1.insert(6, "Ruby")
+
+Lb1.pack()
+top.mainloop()
+"""
---
+++ run_lda.py
@@ -0,0 +1,74 @@
+import sys
+import os
+import numpy
+import json
+import pprint
+
+import lda
+
+import data_path
+
+
+def dict_to_inputs(summoners_to_stats:dict):
+    with open(data_path.file_champion_id_to_name, 'r') as f:
+        id_to_name = json.load(f)
+    with open(data_path.columns_to_id, 'r') as f:
+        columns_to_id = json.load(f)
+    # print(len(id_to_name))
+    # print(len(columns_to_id))
+    X = numpy.zeros((len(summoners_to_stats), len(id_to_name)), dtype='int32')
+    l = 0
+    for summ in summoners_to_stats:
+        d = summoners_to_stats[summ]
+        for c in range(0, len(columns_to_id)):
+            X[l, c] = d.get(str(columns_to_id[str(c)]), 0)
+        l = l + 1
+    ids = [columns_to_id[str(c)] for c in range(0, len(columns_to_id))]
+    vocab = tuple(id_to_name[str(i)] for i in ids)
+    return X, vocab
+
+def lda_inputs(filename):
+    with open(filename) as f:
+        summoners_to_stats = json.load(f)
+    return dict_to_inputs(summoners_to_stats)
+
+def run_lda(filename,filetoname=None,n_topics=10,n_iter=100,n_topic_words=20):
+    if filetoname==None:
+        filetoname=os.path.join(data_path.lda_results,'default'+str(numpy.random.randint(0,100)))
+    fileto = open(filetoname, "w")
+    sys.stdout = fileto
+    sys.stderr = fileto
+    print('Data file: '+str(filename))
+    X, vocab = lda_inputs(filename)
+    model = lda.LDA(n_topics=n_topics, n_iter=n_iter, random_state=1)
+    model.fit(X)
+    topic_word = model.topic_word_
+    for i, topic_dist in enumerate(topic_word):
+        topic_words = numpy.array(vocab)[numpy.argsort(topic_dist)][:-n_topic_words:-1]
+        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
+    fileto.close()
+
+
+if __name__ == '__main__':
+    run_lda(data_path.champions_usage_master)
+
+    # with open('data/champions_usage_master') as f:
+    #     summoners_to_stats = json.load(f)
+    # with open('data/champions_usage_challenger') as f:
+    #     summoners_to_stats.update(json.load(f))
+    # # d = dict()
+    # # for i in list(range(5))+list(range(7,10)):
+    # #     d.update(fromfile('data/champions_usage_plat_'+str(i)))
+    # # summoners_to_stats = d
+    #
+    # X, vocab = dict_to_inputs(summoners_to_stats)
+    # # for i in range(0, len(summoners_to_stats)-1):
+    # #     print(X[i])
+    # # np.savetxt("champ_usage_test.csv", X, delimiter="," )
+    # model = lda.LDA(n_topics=10, n_iter=100, random_state=1)
+    # model.fit(X)
+    # topic_word = model.topic_word_
+    # n_top_words = 20
+    # for i, topic_dist in enumerate(topic_word):
+    #     topic_words = numpy.array(vocab)[numpy.argsort(topic_dist)][:-n_top_words:-1]
+    #     print('Topic {}: {}'.format(i, ' '.join(topic_words)))
