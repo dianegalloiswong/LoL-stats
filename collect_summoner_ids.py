8ee671ae6c1fe0070d276bde8be19acbe7717154
dianegalloiswong <diane.gallois-wong@ens.fr>
2015-10-20 15:17:16 +0200

    Add DataCollector

--- collect_summoner_ids.py
+++ collect_summoner_ids.py
@@ -6,62 +6,83 @@ import abc
 import riotwatcher
 import data_path
 
-report_freq = 100
+default_report_freq = 100
+default_summoner_id = 30890339
+default_n_ids_min = 10
 
 
 
+class DataCollector:
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, riot=None, callback=None, report_freq=default_report_freq):
+        if riot is None:
+            riot = riotwatcher.RiotWatcher()
+        self.riot = riot
+        if callback is None:
+            callback = print
+        self.callback = callback
+        self.report_freq = report_freq
+
+class SummonerIdsCollector(DataCollector):
+    def __init__(self, riot=None, callback=None, report_freq=default_report_freq):
+        DataCollector.__init__(self, riot=riot, callback=callback, report_freq=report_freq)
+
+    def match_id_to_summoner_ids(self, match_id):
+        self.riot.wait()
+        try:
+            match_detail = self.riot.get_match(match_id)
+            return [participantIdentity['player']['summonerId'] for participantIdentity in
+                    match_detail['participantIdentities']]
+        except riotwatcher.LoLException as e:
+            print('LoLException: ' + str(e) + ', in match_id_to_summoner_ids with match_id = ' + str(match_id))
+            return []
+
+    def summoner_id_to_match_ids(self, summoner_id):
+        self.riot.wait()
+        try:
+            match_history = self.riot.get_match_list(summoner_id, ranked_queues=['RANKED_SOLO_5x5'], seasons=['SEASON2015'])
+            # return [match_summary['matchId'] for match_summary in match_history['matches'] if match_summary['season']=='SEASON2015']
+            return [match_summary['matchId'] for match_summary in match_history['matches']]
+        except riotwatcher.LoLException as e:
+            print('LoLException: ' + str(e) + ', in summoner_id_to_match_ids with summoner_id = ' + str(summoner_id))
+            return []
+
+    def collect_summoner_ids(self, n_ids_min, initial_summoner_id):
+        summoner_ids = set()
+        match_ids = set()
+        pending_summoner_ids = [initial_summoner_id]
+        pending_match_ids = []
+        while len(summoner_ids) < n_ids_min and len(pending_summoner_ids) > 0:
+            for s_id in pending_summoner_ids:
+                for m_id in self.summoner_id_to_match_ids(s_id):
+                    if m_id not in match_ids:
+                        match_ids.add(m_id)
+                        pending_match_ids.append(m_id)
+                if 5 * len(match_ids) > n_ids_min:
+                    break
+            pending_summoner_ids.clear()
+            for m_id in pending_match_ids:
+                for s_id in self.match_id_to_summoner_ids(m_id):
+                    if s_id not in summoner_ids:
+                        summoner_ids.add(s_id)
+                        if len(summoner_ids) % self.report_freq == 0:
+                            self.callback('Collecting summoner ids: {}/{} done'.format(len(summoner_ids), n_ids_min))
+                        pending_summoner_ids.append(s_id)
+                if len(summoner_ids) > n_ids_min:
+                    break
+            pending_match_ids.clear()
+        return summoner_ids
 
 
+    def make_summoner_ids(self, initial_id=default_summoner_id, n_min=default_n_ids_min, name=None):
+        if name is None:
+            name = 'init{}min{}'.format(initial_id, n_min)
+        data = self.collect_summoner_ids(n_min, initial_id)
+        data = list(data)
+        summoner_ids = SummonerIds(initial_id, n_min, name, data)
+        return summoner_ids
 
-
-
-def match_id_to_summoner_ids(riot, match_id):
-    riot.wait()
-    try:
-        match_detail = riot.get_match(match_id)
-        return [participantIdentity['player']['summonerId'] for participantIdentity in
-                match_detail['participantIdentities']]
-    except riotwatcher.LoLException as e:
-        print('LoLException: ' + str(e) + ', in match_id_to_summoner_ids with match_id = ' + str(match_id))
-        return []
-
-
-def summoner_id_to_match_ids(riot, summoner_id):
-    riot.wait()
-    try:
-        match_history = riot.get_match_list(summoner_id, ranked_queues=['RANKED_SOLO_5x5'], seasons=['SEASON2015'])
-        # return [match_summary['matchId'] for match_summary in match_history['matches'] if match_summary['season']=='SEASON2015']
-        return [match_summary['matchId'] for match_summary in match_history['matches']]
-    except riotwatcher.LoLException as e:
-        print('LoLException: ' + str(e) + ', in summoner_id_to_match_ids with summoner_id = ' + str(summoner_id))
-        return []
-
-
-def collect_summoner_ids(riot, n, initial_summoner_id):
-    summoner_ids = set()
-    match_ids = set()
-    pending_summoner_ids = [initial_summoner_id]
-    pending_match_ids = []
-    while len(summoner_ids) < n and len(pending_summoner_ids) > 0:
-        for s_id in pending_summoner_ids:
-            for m_id in summoner_id_to_match_ids(riot, s_id):
-                if m_id not in match_ids:
-                    match_ids.add(m_id)
-                    pending_match_ids.append(m_id)
-            if 5 * len(match_ids) > n:
-                break
-        pending_summoner_ids.clear()
-        for m_id in pending_match_ids:
-            for s_id in match_id_to_summoner_ids(riot, m_id):
-                if s_id not in summoner_ids:
-                    summoner_ids.add(s_id)
-                    if len(summoner_ids) % report_freq == 0:
-                        print(str(len(summoner_ids)) + ' done')
-                    pending_summoner_ids.append(s_id)
-            if len(summoner_ids) > n:
-                break
-        pending_match_ids.clear()
-    return summoner_ids
 
 
 
@@ -79,7 +100,8 @@ def collect_master_summoner_ids():
 
 
 
-class DataSet(metaclass=abc.ABCMeta):
+class DataSet:
+    __metaclass__ = abc.ABCMeta
 
     def __init__(self, name, initial_summoner_id, n_ids_min, data):
         self.name = name
@@ -135,17 +157,6 @@ class SummonerIds(DataSet):
         return data_path.summoner_ids_dir
 
     @classmethod
-    def collect(cls, initial_id=30890339, n_min=10, name=None, riot=None):
-        if name is None:
-            name = 'init{}min{}'.format(initial_id, n_min)
-        if riot is None:
-            riot = riotwatcher.RiotWatcher()
-        data = collect_summoner_ids(riot, n_min, initial_id)
-        data = list(data)
-        summoner_ids = SummonerIds(initial_id, n_min, name, data)
-        return summoner_ids
-
-    @classmethod
     def from_dict_and_data(cls, dic, data):
         summoner_ids = SummonerIds(dic['initial_summoner_id'], dic['n_ids_min'], dic['name'], data)
         return summoner_ids
@@ -159,12 +170,35 @@ class SummonerIds(DataSet):
         return d
 
 
-class TestSummonerIds(unittest.TestCase):
+class TestCollectSummonerIds(unittest.TestCase):
+
+    name = 'TestCollectSummonerIds'
+
+    reports = []
+
+    def send_report(self, s):
+        self.reports.append(s)
+
     def test(self):
-        name = 'test'
-        summoner_ids = SummonerIds.collect(name=name)
+        n_min = 30
+        self.reports = []
+        collector = SummonerIdsCollector(report_freq=10, callback=self.send_report)
+        summoner_ids = collector.make_summoner_ids(n_min=n_min, name=self.name)
+        self.assertGreaterEqual(summoner_ids.actual_n_ids, n_min)
+        for i in [1,2,3]:
+            j = 10*i
+            self.assertEqual(self.reports.pop(0), 'Collecting summoner ids: {}/{} done'.format(j, n_min))
+
+
+class TestSummonerIds(unittest.TestCase):
+
+    name = 'test'
+
+    def test(self):
+        collector = SummonerIdsCollector()
+        summoner_ids = collector.make_summoner_ids(name=self.name)
         summoner_ids.dump()
-        summoner_ids1 = SummonerIds.load(name)
+        summoner_ids1 = SummonerIds.load(self.name)
         self.assertEqual(summoner_ids.to_dict(), summoner_ids1.to_dict())
         self.assertEqual(summoner_ids.data, summoner_ids1.data)
 
@@ -172,12 +206,5 @@ if __name__ == "__main__":
 
     unittest.main()
 
-    # name = 'test'
-    # summoner_ids = SummonerIds.collect(name=name)
-    # summoner_ids.dump()
-    # summoner_ids1 = SummonerIds.load(name)
-
-    # import doctest
-    # doctest.testmod()
 
 
